0b98614cbc92b054a9f416b7b93ec3f00ccf95bb
SATZEL02 <gargdev89@gmail.com>
2024-02-10 23:52:34 +0530

    Update Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,20 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import PrivateRoute from './components/PrivateRoute';
+
 export default function App(){
   return(
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
         <Route path="/sign-in" element={<SignIn />}/>
         <Route path="/sign-up" element={<SignUp />}/>
         <Route path="/about" element={<About />}/>
+        <Route element={<PrivateRoute />}>
+        <Route path="/" element={<Home />}/>
         <Route path="/profile" element={<Profile />}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
