77274d6391a1377447063c6d0c0782e9264c2eef
SATZEL02 <gargdev89@gmail.com>
2024-03-16 17:30:19 +0530

    Add ChangeOrigin to vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig(({mode}) =>{
         },
         '/compiler':{
           target: env.VITE_AWS_PUBLIC_IPADDRESS,
+          changeOrigin:true,
           secure:false,
         },
       },
