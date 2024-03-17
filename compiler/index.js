49adfa910db983a669a43434846b4852f6dd8ff8
SATZEL02 <gargdev89@gmail.com>
2024-03-17 17:15:55 +0530

    Fix Compiler Server CORS Policy ERROR

--- compiler/index.js
+++ compiler/index.js
@@ -6,7 +6,6 @@ import codeRouter from './routes/code.route.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
@@ -14,8 +13,29 @@ app.use(express.urlencoded({ extended:true}));
 app.listen(8080, (req,res) => {
     console.log('listening on port 8080!');
 });
-
-
+const prodOrigins = [
+    process.env.ORIGIN_1,
+    process.env.ORIGIN_2
+  ];
+  const allowedOrigins = prodOrigins;
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (process.env.NODE_ENV === 'production') {
+          if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+          } else {
+            callback(new Error(`${origin} not allowed by cors`));
+          }
+        } else {
+          callback(null, true);
+        }
+      },
+      optionsSuccessStatus: 200,
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }),
+  );
 app.use("/compiler/code",codeRouter);
 
 app.use((err,req,res,next) => {
