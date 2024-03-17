8aafd27964e8eec397897be700777199309b111c
SATZEL02 <gargdev89@gmail.com>
2024-03-17 15:25:47 +0530

    Add Vercel.json File

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,28 @@
+// eslint-disable-next-line no-unused-vars
 import { defineConfig,loadEnv} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line no-unused-vars
 export default defineConfig(({ mode })=>{
   
   // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd(), '');
+  // const env = loadEnv(mode, process.cwd(), '');
   return ({
-    server:{
-      proxy: {
-        '/api': {
-          target: env.API_IPADDRESS,
-          changeOrigin:true,
-          secure:true,
-        },
-        '/compiler':{
-          target: env.AWS_PUBLIC_IPADDRESS,
-          changeOrigin:true,
-          secure:false,
-        },
-      },
-    },
+    // server:{
+    //   proxy: {
+    //     '/api': {
+    //       target: env.API_IPADDRESS,
+    //       changeOrigin:true,
+    //       secure:false,
+    //     },
+    //     '/compiler':{
+    //       target: env.AWS_PUBLIC_IPADDRESS,
+    //       changeOrigin:true,
+    //       secure:false,
+    //     },
+    //   },
+    // },
     plugins: [react()]
   });
 });
