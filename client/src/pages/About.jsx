2ad2fda0a13b365c02bf930c1caa03c813052995
SATZEL02 <gargdev89@gmail.com>
2024-03-24 16:36:13 +0530

    Update About Page UI

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,48 +3,48 @@ export default function About() {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800">Welcome to CodeBattle - Where Code Meets Evaluation!</h1>
-      <p className="mb-4 text-slate-700">
+      <p className="mb-4 text-xl text-slate-700">
         At CodeBattle, we've built a cutting-edge online code judge platform that revolutionizes the way developers test, evaluate, and improve their coding skills. Whether you're a seasoned programmer honing your expertise or a novice eager to learn, our platform provides the tools and resources you need to thrive in the ever-evolving world of software development.
       </p>
-      <h2 className="font-semibold mb-4 text-slate-800">Our Mission</h2>
-      <p className="mb-4 text-slate-700">
+      <h2 className="font-semibold text-2xl mb-4 text-slate-800">Our Mission</h2>
+      <p className="mb-4 text-xl text-slate-700">
 
         Our mission at CodeBattle is simple: to empower developers of all levels to reach their full potential. We believe that access to high-quality coding challenges and thorough feedback is essential for growth in the field of programming. That's why we've created a platform that fosters learning, collaboration, and innovation.
       </p>
-      <h2 className="font-semibold mb-4 text-slate-800">What Sets Us Apart</h2>
+      <h2 className="font-semibold text-2xl mb-4 text-slate-800">What Sets Us Apart</h2>
 
-      <p className="mb-4 text-slate-700">
+      <p className="mb-4 text-xl text-slate-700">
 
         Comprehensive Evaluation: Unlike traditional coding challenges, our platform goes beyond simply running test cases. We provide detailed feedback and analysis on your code, helping you understand not only what works but also why it works.
       </p>
-      <p className="mb-4 text-slate-700">
+      <p className="mb-4 text-xl text-slate-700">
         Diverse Range of Challenges: Whether you're interested in algorithms, data structures, web development, or machine learning, we've got you covered. Our extensive library of challenges spans various domains and difficulty levels, ensuring there's something for everyone.
       </p>
-      <p className="mb-4 text-slate-700">
+      <p className="mb-4 text-xl text-slate-700">
         Real-World Simulation: Our platform simulates real-world scenarios, giving you a taste of what it's like to work on actual projects. From simulating client requirements to debugging existing codebases, our challenges mirror the challenges you'll encounter in your professional career.
       </p>
-      <p className="mb-4 text-slate-700">
+      <p className="mb-4 text-xl text-slate-700">
         Community Collaboration: Learning doesn't happen in isolation. That's why we've built a vibrant community of developers where you can connect, share insights, and learn from each other's experiences. Whether you're seeking advice, looking for a coding partner, or simply want to celebrate your achievements, our community is here to support you every step of the way.
       </p>
 
-      <h2 className="font-semibold mb-4 text-slate-800">How It Works</h2>
-      <p className="mb-4 text-slate-700">
+      <h2 className="font-semibold mb-4 text-2xl text-slate-800">How It Works</h2>
+      <p className="mb-4 text-xl text-slate-700">
       Using CodeBattle is straightforward:
       </p>
-      <p className="mb-4 text-slate-700">
+      <p className="mb-4 text-xl text-slate-700">
         Choose a Challenge: Browse our extensive library of challenges and select one that piques your interest.
       </p>
-      <p className="mb-4 text-slate-700">
+      <p className="mb-4 text-xl text-slate-700">
         Code Solution: Write your solution using the programming language of your choice.
       </p>
-      <p className="mb-4 text-slate-700">
+      <p className="mb-4 text-xl text-slate-700">
         Submit Your Code: Submit your code for evaluation.
       </p>
-      <p className="mb-4 text-slate-700">
+      <p className="mb-4 text-xl text-slate-700">
         Iterate and Improve: Use the platform again and again to refine your code and enhance your skills.
       </p>
-      <h2 className="font-semibold mb-4 text-slate-800">Join Us Today!</h2>
-      <p className="mb-4 text-slate-700">
+      <h2 className="font-semibold text-2xl mb-4 text-slate-800">Join Us Today!</h2>
+      <p className="mb-4 text-xl text-slate-700">
         Whether you're looking to sharpen your coding skills, prepare for technical interviews, or simply enjoy the thrill of solving challenging problems, CodeBattle is the ultimate destination for developers. Join our growing community today and embark on a journey of continuous learning and improvement. Let's code together!
       </p>
     </div>
